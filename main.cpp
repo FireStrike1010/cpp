dd962e6543b65538b52fe5bb7495875dc4f188c4
FireStrike1010 <81763537+FireStrike1010@users.noreply.github.com>
2023-01-21 22:40:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,55 @@
 
 using namespace std;
 
-class LongLong{
+
+//Во всех заданиях, помимо указанных в задании операций, обязательно должны быть реализованы следующие методы:
+//– метод инициализации init;
+//– ввод с клавиатуры read;
+//– вывод на экран display;
+//– преобразование в строку toString.
+
+//Все задания должны быть реализованы двумя способами:
+//- тип данных представляется структурой с необходимыми полями, а операции реализуются как внешние функции, которые получают объекты данного типа в качестве аргументов;
+//– как класс с закрытыми полями, где операции реализуются как методы класса.
+
+//18. Создать класс LongLong для работы с целыми числами из 64 бит. Число должно быть представлено двумя полями: long – старшая часть,
+//unsigned long – младшая часть. Должны быть реализованы арифметические операции, присутствующие в С++ (без присваивания), и сравнения.
+
+struct LongLongs{
+    long low;
+    unsigned long high;
+}
+
+
+
+class LongLongc{
     private:
         long low;
         unsigned long high;
     public:
-        LongLong(string input){
+        LongLongc(string input){
+            int neg = 1;
+            if (input[0] == '-'){
+                neg = neg - 2;
+                input = input.substr(1);
+            }
+            if (input.length() < 9){
+                low = stol(input) * neg;
+                high = 0;
+            }
+            else{
+                low = stol(input.substr(input.length() - 9, input.length())) * neg;
+                high = stol(input.substr(0, input.length() - 9));
+            }
+        }
+        LongLongc(){
+            low = 0;
+            high = 0;
+        }
+        void read(){
+            string input{};
+            cout << "Введите число\n";
+            getline(cin >> ws, input);
             int neg = 1;
             if (input[0] == '-'){
                 neg = neg - 2;
@@ -44,23 +87,23 @@ class LongLong{
         long long number(){
             return stoll(get_string());
         }
-        LongLong operator+(LongLong other){
+        LongLongc operator+(LongLong other){
             long long result = this -> number() + other.number();
             return LongLong(to_string(result));
         }
-        LongLong operator-(LongLong other){
+        LongLongc operator-(LongLong other){
             long long result = this -> number() - other.number();
             return LongLong(to_string(result));
         }
-        LongLong operator*(LongLong other){
+        LongLongc operator*(LongLong other){
             long long result = this -> number() * other.number();
             return LongLong(to_string(result));
         }
-        LongLong operator/(LongLong other){
+        LongLongc operator/(LongLong other){
             long long result = this -> number() / other.number();
             return LongLong(to_string(result));
         }
-        LongLong operator%(LongLong other){
+        LongLongc operator%(LongLong other){
             long long result = this -> number() % other.number();
             return LongLong(to_string(result));
         }
